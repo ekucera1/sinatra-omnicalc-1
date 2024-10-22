24c32ed5290c640e2862ae68ed62e5d16cb61793
ekucera1 <ekucera@uchicago.edu>
2024-10-22 20:37:29 +0000

    many changes

--- app.rb
+++ app.rb
@@ -1,6 +1,47 @@
 require "sinatra"
 require "sinatra/reloader"
 
+get("/goodbye") do
+  erb(:bye)
+end
+
+get("/square/new") do
+  erb(:new_square_calc)
+end
+
+get '/square/results' do
+  @the_num = params.fetch("users_number").to_f
+
+  @the_result = @the_num ** 2
+
+  erb(:square_results)
+end
+
+get("/square_root/new") do
+  erb(:new_square_root_calc)
+end
+
+get '/square_root/results' do
+  @the_num = params.fetch("users_number").to_f
+
+  @the_result = @the_num ** 0.5
+
+  erb(:square_root_results)
+end
+
+get("/payment/new") do
+  erb(:new_payment_calc)
+end
+
+get '/payment/results' do
+  @the_num = params.fetch("users_number").to_f
+
+  @the_result = @the_num ** 0.5
+
+  erb(:payment_results)
+end
+
+
 get("/") do
   "
   <h1>Welcome to your Sinatra App!</h1>
